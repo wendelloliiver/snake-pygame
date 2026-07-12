dbcff897464ed98f5a74587d5c720e23e641d325
Wendell Oliveira <wendelloliver.dev@gmail.com>
2026-07-11 22:54:08 -0300

    init snake game with python and pygame

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+
+if __name__ == '__main__':
+    pygame.init()
+
+
